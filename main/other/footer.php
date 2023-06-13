48a9219ca54b2d3e3c49083a1c84c3a7ec2ed32e
lushaosalt <121482361+lushaosalt@users.noreply.github.com>
2023-06-13 19:44:20 +0300

    Delete main directory

--- main/other/footer.php
+++
@@ -1,7 +0,0 @@
-		<div class="footer">
-			<a href="https://github.com/avitld/SearchTLD">Source</a>
-			<a href="./settings.php">Config</a>
-			<a href="./donate.php">Donate</a>
-		</div>
-	</body>
-</html>
